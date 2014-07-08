0ce617c9ab9d34864d6cf2bb301a2d35bcf6c491
root <root@www3057uo.sakura.ne.jp>
2014-07-08 19:32:15 +0900

    unicornのソケットを/var/run/animefolio_unicorn.sockに変更

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,8 +1,11 @@
 worker_processes 2
 
 project_root = File.expand_path("../../../", __FILE__)
-listen "#{project_root}/tmp/unicorn.sock"
-pid "#{project_root}/tmp/pids/unicorn.pid"
+#listen "#{project_root}/tmp/unicorn.sock"
+#pid "#{project_root}/tmp/pids/unicorn.pid"
+
+listen "/var/run/animefolio_unicorn.sock"
+pid "/var/run/animefolio_unicorn.pid"
 
 stderr_path "#{project_root}/log/unicorn_err.log"
 stdout_path "#{project_root}/log/unicorn.log"
