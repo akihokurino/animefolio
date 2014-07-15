0c5e92c317e40c8175bbe562d410762a2316e87e
root <root@www3057uo.sakura.ne.jp>
2014-07-16 00:48:30 +0900

    本番環境向けに、env.jsのHOSTを変更した

--- ng-app/app/scripts/factories/env.js
+++ ng-app/app/scripts/factories/env.js
@@ -1,5 +1,6 @@
 angular.module("animefolio").factory("env", function(){
-  var HOST = "http://127.0.0.1:3000";
+  //var HOST = "http://127.0.0.1:3000";
+  var HOST = "http://animefolio.com/api";
 
   return {
     getHost: function(){
